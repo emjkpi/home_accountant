88d14eebbdb5990135622a9787154c2e90407dfb
emzar.kvitsyniia <emzar.kvitsyniia@pragmaticplay.com>
2023-11-08 11:04:54 +0100

    Spring+Gradle

---
+++ homeaccountant/src/main/java/com/example/homeaccountant/HomeaccountantApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeaccountant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeaccountantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeaccountantApplication.class, args);
+	}
+
+}
---
+++ homeaccountant/src/test/java/com/example/homeaccountant/HomeaccountantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeaccountant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeaccountantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

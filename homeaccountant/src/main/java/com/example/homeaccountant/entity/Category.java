03832ec5ad53ec1c9ee5d3a34dbec245e0072175
emzar.kvitsyniia <emzar.kvitsyniia@pragmaticplay.com>
2023-11-23 00:24:03 +0100

    add entity

---
+++ homeaccountant/src/main/java/com/example/homeaccountant/entity/Category.java
@@ -0,0 +1,21 @@
+package com.example.homeaccountant.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Expense> expenses;
+
+
+}
---
+++ homeaccountant/src/main/java/com/example/homeaccountant/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.homeaccountant.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Expense> expenses;
+
+}

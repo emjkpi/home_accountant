816d5fe94ba7b5070a5e392f17849af29e7b5504
emzar.kvitsyniia <emzar.kvitsyniia@pragmaticplay.com>
2023-11-24 18:25:05 +0100

    add JacksonConfig

--- homeaccountant/src/main/java/com/example/homeaccountant/controller/MyRESTController.java
+++ homeaccountant/src/main/java/com/example/homeaccountant/controller/MyRESTController.java
@@ -4,12 +4,15 @@ import com.example.homeaccountant.entity.User;
 import com.example.homeaccountant.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class MyRESTController {
+
+    private final Logger logger = LoggerFactory.getLogger(MyRESTController.class);
 
     @Autowired
     private UserService userService;
@@ -17,6 +20,7 @@ public class MyRESTController {
     @GetMapping("/users")
     public List<User> showAllUsers() {
         List<User> allUsers = userService.getAllUsers();
+        logger.info("Retrieved users: {}", allUsers);
         return allUsers;
     }
 }
--- homeaccountant/src/main/java/com/example/homeaccountant/entity/User.java
+++ homeaccountant/src/main/java/com/example/homeaccountant/entity/User.java
@@ -1,7 +1,6 @@
 package com.example.homeaccountant.entity;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -21,7 +20,43 @@ public class User {
 
     private String lastName;
 
-//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-//    private List<Expense> expenses;
+    public Long getUserId() {
+        return userId;
+    }
 
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
